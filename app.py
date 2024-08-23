58042cdff0d169922a47d0aa0d7a08a24d51c791
Rachana Kulkarni <136351173+rachanakure@users.noreply.github.com>
2024-08-23 12:54:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,8 @@ def leaderboard():
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
